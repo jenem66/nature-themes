a7493cf7dd8c14070ae8aaae979739f1ad4da70b
Jenelle <jenem66@gmail.com>
2021-07-06 20:04:38 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var themeList = [
+  "Fish",
+  "Water",
+  "Sand",
+  "Palm tree",
+  "Stars",
+  "Butterflies", 
+  "Flowers",
+  "Sun",
+  "Rainbow",
+  "Cotton",
+  "Breeze",
+  "Forest",
+  "Fruit",
+  "Rock",
+  "Lake",
+  "Mountain",
+  "Birds",
+  "Sea Shells",
+  "Ocean",
+  "Pine tree",
+  "Snow",
+  "Sky",
+  "Grass",
+  "Cloud",
+  "Mint",
+  "Pine cones",
+  "Sand dune",
+  "River",
+  "Feather",
+  "Cactus",
+  "Leaf"
+  ];
+  
+  var getTheme = function () {
+    return themeList[Math.floor(Math.random() * themeList.length)];
+  };
+  
+  $("#theButton").click(function () {
+    $(".theme").html(getTheme());
+  });
+  
+
+
